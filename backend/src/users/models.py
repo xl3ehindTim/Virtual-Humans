29e9c385f980a6632a7dc677d44ccd30ca1bb7e6
xl3ehindTim <timbrouwers1@gmail.com>
2024-11-07 08:37:16 +0100

    update user

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -15,9 +15,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    embedding = models.BinaryField(null=True, blank=True)
 
+    is_staff = models.BooleanField(default=False)
+    
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
     REQUIRED_FIELDS: list[str] = []
