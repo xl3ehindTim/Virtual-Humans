cb80a27b26bc38ec2f73d24fe8ec642e8e318491
xl3ehindTim <timbrouwers1@gmail.com>
2024-11-29 14:08:46 +0100

    Automatically import consumers

--- backend/src/events/apps.py
+++ backend/src/events/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class EventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
+
+    def ready(self) -> None:
+        """
+        Override app configuration to load consumers
+        """
+        import events.consumers
