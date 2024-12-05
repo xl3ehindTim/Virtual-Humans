56f613c0aa2bbcb20b014735a7f76582e885b94b
xl3ehindTim <timbrouwers1@gmail.com>
2024-12-05 11:16:01 +0100

    Modify event names

--- backend/src/events/subscribers.py
+++ backend/src/events/subscribers.py
@@ -7,15 +7,13 @@ def initialize_listeners():
     Subscribes event handlers to the event bus.
     """
 
-    # Subscribe handlers
-    event_bus.subscribe("event.image", process_emotions)
-    event_bus.subscribe("event.image", face_recognition)
+    event_bus.subscribe("video.frame", process_emotions)
+    event_bus.subscribe("video.frame", face_recognition)
     
-    event_bus.subscribe("event.text", generate_response)
+    event_bus.subscribe("audio.transcription", generate_response)
     event_bus.subscribe("event.save", save_event)
 
-    # Start listeners for each event type 
-    event_bus.start_listener("event.image") # rename: video.frame?
-    event_bus.start_listener("event.text") # rename: audio.transcription?
+    event_bus.start_listener("video.frame") 
+    event_bus.start_listener("audio.transcription") 
     event_bus.start_listener("event.save")
     event_bus.start_listener("event.virtual_human")
